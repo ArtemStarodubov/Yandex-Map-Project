a727a7ee224a003381847ed1e0b65b3afc3c3725
ArtemStarodubov <57946120+ArtemStarodubov@users.noreply.github.com>
2020-03-03 16:45:51 +0400

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import pygame
+import requests
+
+position = (133.795384, -25.694768)
+delta = 30
+
+params = {
+    'll': ",".join(map(str, position)),
+    'spn': f'{delta},{delta}',
+    'l': 'sat',
+}
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=&spn=30,30&l=sat"
+response = requests.get(map_request, params=params)
+
+if response.status_code != 200:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    for event in pygame.event.get():
+        if event.key == pygame.K_PAGEUP:
+            delta += 3
+
+        if event.key == pygame.K_PAGEDOWN:
+            delta -= 3
+pygame.quit()
+
+os.remove(map_file)
